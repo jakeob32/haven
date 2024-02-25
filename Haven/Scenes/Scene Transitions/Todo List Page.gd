e39bdd36ed0f597029ca3d020d03a715dfc9e562
meesh3ll <119637650+meesh3ll@users.noreply.github.com>
2024-02-25 10:51:59 -0800

    kinda working task list?

--- Haven/Scenes/Scene Transitions/Todo List Page.gd
+++ Haven/Scenes/Scene Transitions/Todo List Page.gd
@@ -1,10 +1,37 @@
 extends Control
-@export var task_entry_scene : PackedScene
 # Called when the node enters the scene tree for the first time.
+var _task_entry_scene = preload("res://Scenes/TaskEntry.tscn")
+var one_task = {
+	"currency" : 0,
+	"description" : "",
+	"name" : ""
+}
+
+func import_tasks():
+	Global.UserTodo["userID"] = Global.userID
+	Global.UserTodo["tasks"].append("do_work")
+	Global.UserTodo["tasks"].append("drink_water")
+	Global.UserTodo["tasks"].append("eat_fruit")
+	Global.UserTodo["tasks"].append("exercise_daily")
+	for i in range(len(Global.UserTodo["tasks"])):
+		Global.UserTodo["isComplete"].append(false)
+	Global.update_doc_fields("UserTodo", Global.userID, Global.UserTodo)
+
+func update_one_task(i: int):
+	one_task = await Global.get_doc_fields("Task", Global.UserTodo["tasks"][i])
+	return one_task
+	print(one_task)
+	
 func _ready():
-	var control_instance: Control = task_entry_scene.instantiate() 
-
-
+	if (Global.UserTodo["tasks"] == []):
+		import_tasks()
+	for i in range(len(Global.UserTodo["tasks"])):
+		one_task = await update_one_task(i)
+		var task = _task_entry_scene.instantiate()
+		$Panel/Control/task_container.add_child(task)
+		task.currency_label.text = str(one_task["currency"])
+		task.name_label.text = one_task["name"]
+		task.description_label.text = one_task["description"]
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
---
+++ Haven/Scenes/TaskEntry.gd
@@ -0,0 +1,16 @@
+extends HBoxContainer
+@onready var currency_label: Label = $currency_label
+@onready var name_label: Label = $name_label
+@onready var description_label: Label = $description_label
+@onready var check_button: TextureButton = $CheckButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_check_button_pressed():
+	pass
