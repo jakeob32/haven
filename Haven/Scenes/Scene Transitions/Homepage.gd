7d5403c8d3f5d363044b1a1651b1115223900d2e
pineapplezsauce <142255538+pineapplezsauce@users.noreply.github.com>
2024-02-25 10:50:25 -0800

    just removed some extra stuff

--- Haven/Scenes/Scene Transitions/Homepage.gd
+++ Haven/Scenes/Scene Transitions/Homepage.gd
@@ -1,30 +1,16 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_gacha_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Gacha Page.tscn");
-
 
 func _on_todo_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Todo List Page.tscn");
 
-
 func _on_logout_button_pressed():
 	Firebase.Auth.logout()
 	get_tree().change_scene_to_file("res://Scenes/Login Page.tscn")
-
-# testing update_doc_fields()
-func _on_button_pressed():
-	Global.UserFurniture["furniture"].append("abc")
-	Global.update_doc_fields("UserFurniture", Global.userID, Global.UserFurniture)
 
