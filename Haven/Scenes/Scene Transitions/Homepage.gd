49c0493f1a5e7ee7293ec431cfd4fff2ec2c9b93
pineapplezsauce <142255538+pineapplezsauce@users.noreply.github.com>
2024-02-25 12:20:29 -0800

    made general layout for homepage

--- Haven/Scenes/Scene Transitions/Homepage.gd
+++ Haven/Scenes/Scene Transitions/Homepage.gd
@@ -2,7 +2,25 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", _http_request_completed)
+	
+	var error = http_request.request("https://firebasestorage.googleapis.com/v0/b/haven-f687f.appspot.com/o/dorm_assets%2FChair.png?alt=media&token=9ca80b2b-3b2b-4d03-b481-9552c5fb1b7e")
+	if error != OK:
+		print("bruh theres an errorrrrr")
+
+func _http_request_completed(result, response_code, headers, body) :
+	var image = Image.new()
+	var error = image.load_png_from_buffer(body)
+	if error != OK:
+		print("erorrrr")
+	
+	var texture = ImageTexture.new()
+	texture.create_from_image(image)
+	$room.texture = texture
+	
+
 
 func _on_gacha_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Gacha Page.tscn");
