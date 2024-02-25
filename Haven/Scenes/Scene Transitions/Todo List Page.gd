2842dee225a61790d66d65baba55989f5103a214
meesh3ll <119637650+meesh3ll@users.noreply.github.com>
2024-02-25 14:03:18 -0800

    working task list

--- Haven/Scenes/Scene Transitions/Todo List Page.gd
+++ Haven/Scenes/Scene Transitions/Todo List Page.gd
@@ -6,13 +6,14 @@ var one_task = {
 	"description" : "",
 	"name" : ""
 }
-
 func import_tasks():
 	Global.UserTodo["userID"] = Global.userID
 	Global.UserTodo["tasks"].append("do_work")
 	Global.UserTodo["tasks"].append("drink_water")
 	Global.UserTodo["tasks"].append("eat_fruit")
 	Global.UserTodo["tasks"].append("exercise_daily")
+	Global.UserTodo["tasks"].append("go_offline")
+	Global.UserTodo["tasks"].append("take_vitamins")
 	for i in range(len(Global.UserTodo["tasks"])):
 		Global.UserTodo["isComplete"].append(false)
 	Global.update_doc_fields("UserTodo", Global.userID, Global.UserTodo)
@@ -20,22 +21,28 @@ func import_tasks():
 func update_one_task(i: int):
 	one_task = await Global.get_doc_fields("Task", Global.UserTodo["tasks"][i])
 	return one_task
-	print(one_task)
 	
 func _ready():
 	if (Global.UserTodo["tasks"] == []):
 		import_tasks()
+	print(Global.UserTodo)
+	Global.update_doc_fields("UserTodo", Global.userID, Global.UserTodo)
+	Global.update_doc_fields("UserMoney", Global.userID, Global.UserMoney)
 	for i in range(len(Global.UserTodo["tasks"])):
+		await update_one_task(i)
 		one_task = await update_one_task(i)
 		var task = _task_entry_scene.instantiate()
 		$Panel/Control/task_container.add_child(task)
+		task.current_task = i
+		if (Global.UserTodo["isComplete"][i] == true):
+			task.isComplete = true
 		task.currency_label.text = str(one_task["currency"])
 		task.name_label.text = one_task["name"]
 		task.description_label.text = one_task["description"]
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_home_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Homepage.tscn")
--- Haven/Scenes/TaskEntry.gd
+++ Haven/Scenes/TaskEntry.gd
@@ -3,14 +3,27 @@ extends HBoxContainer
 @onready var name_label: Label = $name_label
 @onready var description_label: Label = $description_label
 @onready var check_button: TextureButton = $CheckButton
-
+@onready var isComplete = false
+@onready var current_task = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if isComplete == true:
+		$CheckButton.texture_normal = load("res://istockphoto-1435212785-612x612 (1).jpg")
+		$CheckButton.disabled = true
+		set_process(false)
 
-func _on_check_button_pressed():
-	pass
+func _on_check_button_toggled(toggled_on):
+	if isComplete == false:
+		Global.UserTodo["isComplete"][current_task] = true
+		Global.update_doc_fields("UserTodo", Global.userID, Global.UserTodo)
+		Global.currency += int(currency_label.text)
+		Global.UserMoney["userID"] = Global.userID
+		Global.UserMoney["currency"] = Global.currency
+		Global.update_doc_fields("UserMoney", Global.userID, Global.UserMoney)
+		$CheckButton.texture_normal = load("res://istockphoto-1435212785-612x612 (1).jpg")
+		$CheckButton.disabled = true
+	
