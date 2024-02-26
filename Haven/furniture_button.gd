3960f2eaeb0ec8efad8a0a10f0521aabfd9b65ec
pineapplezsauce <142255538+pineapplezsauce@users.noreply.github.com>
2024-02-25 20:06:18 -0800

    added to make the furniture load in properly

--- Haven/furniture_button.gd
+++ Haven/furniture_button.gd
@@ -1,6 +1,9 @@
 extends Button
 @onready var furniture_img: TextureRect = $TextureRect
 @onready var furniture_name: Label = $furniture_name
+var furniture_doc_names = ["dorm_bed", "dorm_chair", "dorm_closet", "dorm_lamp", "dorm_painting", 
+						"dorm_plant", "dorm_poster", "dorm_round_mirror", "dorm_rug", "dorm_shelf", 
+						"dorm_square_mirror", "dorm_table"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +16,7 @@ func _process(delta):
 
 
 func _on_pressed():
+	# if furniture is already placed, the dont show it anymore; else show the furniture
 	print("furniture button pressed")
+	var ref = self.get_parent().get_parent().get_parent().get_parent().get_parent()
+	ref.set_visibility($furniture_name.text)
